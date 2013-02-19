0bd0a56b91a796be2bbc9ae5ebd0bade2eca90e2
Hugh Nougher <hughnougher@gmail.com>
2013-02-19 17:41:07 +1100

    ajax.php now can do multiple queries in one go and selector makes use of this for page numbers.

--- ajax.php
+++ ajax.php
@@ -19,60 +19,79 @@ header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT
 session_start();
 
 // Non-changable constants
-if (isset($_SESSION['showDebug']) && $_SESSION['showDebug'] == 1)
-	define('DEBUG', true);
-if (isset($_SESSION['showStats']) && $_SESSION['showStats'] == 1)
-	define('STATS', true);
+#if (isset($_SESSION['showDebug']) && $_SESSION['showDebug'] == 1)
+#	define('DEBUG', true);
+#if (isset($_SESSION['showStats']) && $_SESSION['showStats'] == 1)
+#	define('STATS', true);
 
-// Check that there is a reason for them to be here and the reason for being here is valid
-if (empty($_REQUEST['q']) || empty($_SESSION['AJAX_QUERIES']) || empty($_SESSION['AJAX_QUERIES'][$_REQUEST['q']]))
-	die('Error: Requested stored query does not exist.');
-$QUERY = $_SESSION['AJAX_QUERIES'][$_REQUEST['q']];
-
-// Prepare PARAM array
-$PARAM = array();
-if (isset($_REQUEST['p'])) {
-	foreach ((array) $_REQUEST['p'] as $k => $p)
-		$PARAM[] = &$_REQUEST['p'][$k]; // Looks nasty but saves mem
-}
-
-// Now we start to get going
 require_once 'config.php';
 require_once CLASS_PATH. '/ErrorHandler.class.php';
 require_once CLASS_PATH. '/HNMySQL.class.php';
 date_default_timezone_set(SERVER_TIMEZONE);
 HNMySQL::connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DATABASE);
 
-// Prepare the statement for use
-$stmt = HNMySQL::prepare($QUERY[0]);
-
-// Check that there is enough parameters sent to execute the query
-if ($stmt->param_count != count($PARAM))
-	die('Error: Requested stored query needs ' .$stmt->param_count. ' parameters, ' .count($PARAM). ' given.');
-
-// Bind the parameters
-if ($stmt->param_count > 0) {
-	array_unshift($PARAM, $QUERY[1]);
-	call_user_func_array(array($stmt, 'bind_param'), $PARAM);
+// Check all queries and params for problems and fix them up
+for ($i = 0; !empty($_REQUEST['q'.($i?$i:'')]); $i++) {
+	$j = ($i ? $i : '');
+	if (empty($_SESSION['AJAX_QUERIES']) || empty($_SESSION['AJAX_QUERIES'][$_REQUEST['q'.$j]]))
+		die('Error: Requested stored query ' .$i. ' does not exist.');
+	$_REQUEST['q'.$j] = $_SESSION['AJAX_QUERIES'][$_REQUEST['q'.$j]];
+	if (!isset($_REQUEST['p'.$j]))
+		$_REQUEST['p'.$j] = array();
+	$_REQUEST['p'.$j] = (array) $_REQUEST['p'.$j];
+	$ACTPARAM = count($_REQUEST['p'.$j]);
+	$REQPARAM = strlen($_REQUEST['q'.$j][1]);
+	if ($REQPARAM != $ACTPARAM)
+		die('Error: Requested stored query ' .$i. ' needs ' .$REQPARAM. ' parameters, ' .$ACTPARAM. ' given.');
 }
+$totalQueries = $i;
 
-// Execute
-if (!$stmt->execute())
-	die('Error: Statement failed to execute.');
-
-// Collect result into JSON
+// Start outer array of JSON
 echo '[';
-$doneFirst = false;
-$result = $stmt->get_result();
-while ($row = $result->fetch_row()) {
-	if ($doneFirst)
-		echo ',';
-	else
-		$doneFirst = true;
-	echo json_encode($row);
-}
-echo ']';
 
-// Clean up for the sake of it
-$result->free();
-$stmt->close();
+for ($i = 0; $i < $totalQueries; $i++) {
+	$j = ($i ? $i : '');
+	$QUERY = $_REQUEST['q'.$j];
+
+	// Prepare PARAM array
+	$PARAM = array();
+	if (isset($_REQUEST['p'.$j])) {
+		foreach ((array) $_REQUEST['p'.$j] as $k => $p)
+			$PARAM[] = &$_REQUEST['p'.$j][$k]; // Looks nasty but saves mem
+	}
+
+	// Prepare the statement for use
+	$stmt = HNMySQL::prepare($QUERY[0]);
+
+	// Bind the parameters
+	if ($stmt->param_count > 0) {
+		array_unshift($PARAM, $QUERY[1]);
+		call_user_func_array(array($stmt, 'bind_param'), $PARAM);
+	}
+
+	// Execute
+	if (!$stmt->execute())
+		die('Error: Statement failed to execute.');
+
+	// Collect result into JSON
+	if ($i > 0)
+		echo ',';
+	echo '[';
+	$doneFirst = false;
+	$result = $stmt->get_result();
+	while ($row = $result->fetch_row()) {
+		if ($doneFirst)
+			echo ',';
+		else
+			$doneFirst = true;
+		echo json_encode($row);
+	}
+	echo ']';
+
+	// Clean up for the sake of it
+	$result->free();
+	$stmt->close();
+}
+
+// End outer array of JSON
+echo ']';
--- hntpl/selector.php
+++ hntpl/selector.php
@@ -22,19 +22,21 @@ require_once TEMPLATE_PATH. '/core.php';
 class HNTPLSelector extends HNTPLCore
 {
 	// Just displays the query on one page
-	const TYPE_BASIC = 'basic';
+	const TYPE_BASIC = 0x0000;
 	// Just displays the query with pagination
-	const TYPE_BASICPAGE = 'basicp';
+	const TYPE_PAGE = 0x0001;
+	const TYPE_BASICPAGE = 0x0001;
 	// Displays a search box which sends one parameter to the query via AJAX
-	const TYPE_SEARCH = 'search';
+	const TYPE_SEARCH = 0x0002;
 	// Same as TYPE_SEARCH with pagination
-	const TYPE_SEARCHPAGE = 'searchp';
+	const TYPE_SEARCHPAGE = 0x0003;
 	#const TYPE_FILTER = 'filter';
 
 	private $type;
 	private $queryCode;
 	private $uri;
 	private $fieldNames;
+	private $queryCodeCounter;
 
 	private $emptyText = 'Enter some text above to start searching.';
 	private $noResultText = 'Nothing was found with the search term.';
@@ -80,6 +82,17 @@ class HNTPLSelector extends HNTPLCore
 	public function set_noresult_text($text) {
 		$this->noResultText = $text;
 	}
+	
+	/**
+	* Sets the query code used for getting the total number of pages in the set.
+	*
+	* @param string $queryCodeCounter
+	*    The query should have the same number of parameters in it as the main one minus the two for LIMIT.
+	* @uses $queryCodeCounter
+	*/
+	public function set_query_code_counter($code) {
+		$this->queryCodeCounter = $code;
+	}
 
 	/**
 	* Default output function. Outputs all the tags that have been added in default format.
@@ -95,23 +108,51 @@ class HNTPLSelector extends HNTPLCore
 		if($capture)
 			ob_start();
 
+		$pageChanger = '';
+		if ($this->type & self::TYPE_PAGE) {
+			if (empty($this->queryCodeCounter)) {
+				echo "Selector cannot display because there is no query set for getting page count.";
+				if ($capture)
+					return ob_get_clean();
+				return;
+			}
+			
+			$pageChanger .= '<div class="pageCtrl" style="display:none">';
+			$pageChanger .= '<button class="pageCtrlFirst">First</button>';
+			$pageChanger .= '<button class="pageCtrlPrev">Previous</button>';
+			$pageChanger .= '<span class="pageCtrlInfo"></span>';
+			$pageChanger .= '<button class="pageCtrlNext">Next</button>';
+			$pageChanger .= '<button class="pageCtrlLast">Last</button>';
+			$pageChanger .= '</div>';
+		}
 ?>
 
 
 <form id="selector<?php echo $this->queryCode; ?>" class="selector" method="post" <?php echo self::attributes($attrib); ?>>
-	<input type="hidden" name="q" value="<?php echo $this->queryCode; ?>"/>
-	<?php if ($this->type == self::TYPE_SEARCH || $this->type == self::TYPE_SEARCHPAGE) { ?>
+	<?php if ($this->type & self::TYPE_SEARCH) { ?>
 		<table class="selector_searchbox"><tr>
 			<td>Enter some search text:</td>
-			<td><input type="text" name="p[]" id="srch" autocomplete="off"/></td>
+			<td><input type="text" id="srch" autocomplete="off"/></td>
 			<td><input type="submit" value="Search"/></td>
 		</tr></table>
-	<?php } ?>
+	<?php }
+	echo $pageChanger; ?>
 	<table>
 		<tr><td id="content" colspan="3">
 			<i><?php echo $this->emptyText; ?></i>
 		</td></tr>
 	</table>
+	<?php echo $pageChanger; ?>
+	
+	<input type="hidden" name="q" value="<?php echo $this->queryCode; ?>"/>
+	<input type="hidden" name="p[]" class="srchp" value=""/>
+	<?php if ($this->type & self::TYPE_PAGE) { ?>
+		<input type="hidden" name="p[]" id="rowoffset" value="0"/>
+		<input type="hidden" name="p[]" id="perpage" value="30"/>
+		<input type="hidden" id="totalrows" value="0"/>
+		<input type="hidden" name="q1" value="<?php echo $this->queryCodeCounter; ?>"/>
+		<input type="hidden" name="p1[]" class="srchp" value=""/>
+	<?php } ?>
 </form>
 
 <script type="text/javascript">
@@ -121,11 +162,15 @@ class HNTPLSelector extends HNTPLCore
 	var selector_delay = function() {
 		if (selector_timeout)
 			window.clearTimeout(selector_timeout);
-		selector_timeout = window.setTimeout(selector, 500);
+		selector_timeout = window.setTimeout(selector, 250);
 	};
 	var selector = function(e) {
 		window.clearTimeout(selector_timeout);
-		$.get("<?php echo SERVER_ADDRESS; ?>/ajax.php", $selector.serialize(), selector2, "json");
+		if ($selector.find(".srchp").val() != $selector.find("#srch").val()) {
+			$selector.find(".srchp").val($selector.find("#srch").val());
+			$selector.find("#rowoffset").val(0);
+		}
+		$.post("<?php echo SERVER_ADDRESS; ?>/ajax.php", $selector.serialize(), selector2, "json");
 		if (e) e.preventDefault();
 		return false;
 	};
@@ -134,50 +179,130 @@ class HNTPLSelector extends HNTPLCore
 		if (data.length == 0) {
 			out += "<i><?php echo $this->noResultText; ?></i>";
 		} else {
-			out += "<table>";
-			<?php
+			var rowdata = data[0];
+			if (rowdata.length == 0) {
+				out += "<i><?php echo $this->noResultText; ?></i>";
+			} else {
+				out += "<table>";
+				<?php
 
-			if (count($this->fieldNames) > 0) {
-				echo 'out += "<tr>';
-				foreach ($this->fieldNames AS $field) {
-					echo '<th>' .$field. '</th>';
+				if (count($this->fieldNames) > 0) {
+					echo 'out += "<tr>';
+					foreach ($this->fieldNames AS $field) {
+						echo '<th>' .$field. '</th>';
+					}
+					echo '</tr>";';
 				}
-				echo '</tr>";';
-			}
 
-			?>
-			var i, j;
-			for (i = 0; i < data.length; i++) {
-				var pointTo = "<?php
-					$tmp = explode('--ID--', $this->uri, 2);
-					echo $tmp[0];
-					echo '" + data[i][0] + "';
-					if (isset($tmp[1]))
-						echo $tmp[1];
-					?>";
+				?>
+				var i, j;
+				for (i = 0; i < rowdata.length; i++) {
+					var pointTo = "<?php
+						$tmp = explode('--ID--', $this->uri, 2);
+						echo $tmp[0];
+						echo '" + rowdata[i][0] + "';
+						if (isset($tmp[1]))
+							echo $tmp[1];
+						?>";
 
-				// If IE6 Hacks Enabled
-				var ieHack = "";
-				if (typeof(window["IERowMouseOver"]) != "undefined")
-					ieHack = " onmouseover='IERowMouseOver()' onmouseout='IERowMouseOut()' style='cursor: pointer'";
+					// If IE6 Hacks Enabled
+					var ieHack = "";
+					if (typeof(window["IERowMouseOver"]) != "undefined" && typeof(window["IERowMouseOut"]) != "undefined")
+						ieHack = " onmouseover='IERowMouseOver()' onmouseout='IERowMouseOut()' style='cursor: pointer'";
 
-				out += "<tr onclick='document.location = \"" + pointTo + "\"'" + ieHack + ">";
-				for (j = 1; j < data[i].length; j++) {
-					if (data[i][j] == null)
-						data[i][j] = "";
-					out += "<td style='text-align: center'>" + data[i][j] + "</td>";
+					out += "<tr onclick='document.location = \"" + pointTo + "\"'" + ieHack + ">";
+					for (j = 1; j < rowdata[i].length; j++) {
+						if (rowdata[i][j] == null)
+							rowdata[i][j] = "";
+						out += "<td style='text-align: center'>" + rowdata[i][j] + "</td>";
+					}
+					out += "</tr>";
 				}
-				out += "</tr>";
+				out += "</table>";
 			}
-			out += "</table>";
+			
+			if (data.length >= 2) {
+				// Page Information
+				$selector.find("#totalrows").val(data[1][0][0]);
+			}
 		}
-
+		
+		<?php if ($this->type & self::TYPE_PAGE) echo "pageCtrlUpdate();"; ?>
 		$selector.find("#content").html(out);
 	};
+	<?php if ($this->type & self::TYPE_PAGE) { ?>
+	function pageCtrl(type) {
+		var $rowoffset = $selector.find("#rowoffset");
+		var rowoffset = parseInt($rowoffset.val());
+		var $perpage = $selector.find("#perpage");
+		var perpage = parseInt($perpage.val());
+		var $totalrows = $selector.find("#totalrows");
+		var totalrows = parseInt($totalrows.val());
+		
+		switch (type) {
+		case "first":
+			if (rowoffset == 0)
+				return; // Nothing to do
+			rowoffset = 0;
+			break;
+		case "prev":
+			if (rowoffset == 0)
+				return; // Nothing to do
+			rowoffset = rowoffset - perpage;
+			break;
+		case "next":
+			rowoffset = rowoffset + perpage;
+			break;
+		case "last":
+			var tmp = Math.floor((totalrows - 1) / perpage) * perpage;
+			if (tmp == rowoffset)
+				return; // Nothing to do
+			rowoffset = tmp;
+			break;
+		default:
+			return; // Nothing to do
+		}
+		if (rowoffset < 0)
+			rowoffset = 0;
+		if (rowoffset >= totalrows)
+			return; // Cant go here
+		
+		$rowoffset.val(rowoffset);
+		selector();
+	};
+	function pageCtrlUpdate() {
+		var rowoffset = parseInt($selector.find("#rowoffset").val());
+		var perpage = parseInt($selector.find("#perpage").val());
+		var totalrows = parseInt($selector.find("#totalrows").val());
+		var curpage = Math.floor(rowoffset / perpage) + 1;
+		var totalpage = Math.floor((totalrows - 1) / perpage) + 1;
+		
+		$selector.find(".pageCtrlInfo").html("Page " + curpage + " of " + totalpage);
+
+		if (totalpage == 1 && $selector.find("#srch").val() == "")
+			$selector.find(".pageCtrl").hide();
+		else
+			$selector.find(".pageCtrl").show();
+		if (curpage == 1)
+			$selector.find(".pageCtrlFirst,.pageCtrlPrev").attr("disabled", "disabled");
+		else
+			$selector.find(".pageCtrlFirst,.pageCtrlPrev").removeAttr("disabled");
+		if (curpage == totalpage)
+			$selector.find(".pageCtrlNext,.pageCtrlLast").attr("disabled", "disabled");
+		else
+			$selector.find(".pageCtrlNext,.pageCtrlLast").removeAttr("disabled");
+	};
+	<?php } ?>
 
 	$(document).ready(function() {
-		$selector.find("#srch").bind("keyup", selector_delay);
-		$selector.bind("submit", selector);
+		$selector.on("submit", selector);
+		$selector.on("keyup", "#srch", selector_delay);
+		<?php if ($this->type & self::TYPE_PAGE) { ?>
+		$selector.on("click", ".pageCtrlFirst", function(){pageCtrl("first");return false});
+		$selector.on("click", ".pageCtrlPrev", function(){pageCtrl("prev");return false});
+		$selector.on("click", ".pageCtrlNext", function(){pageCtrl("next");return false});
+		$selector.on("click", ".pageCtrlLast", function(){pageCtrl("last");return false});
+		<?php } ?>
 
 		// Preload
 		selector();
